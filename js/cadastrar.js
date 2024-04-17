57bc62ac7b8a50b0d07ff2fdc5e91d02a52167d6
DanilOliveir4 <danibrx125@gmail.com>
2024-04-17 14:27:42 -0300

    Utilizando LocalStorage

--- js/cadastrar.js
+++ js/cadastrar.js
@@ -1,31 +1,52 @@
 document.querySelector("#botao-cadastrar").addEventListener("click", () => {
+  const form = document.querySelector("form");
 
+  const tarefa = {
+    titulo: form.titulo.value,
+    descricao: form.descricao.value,
+    pontos: form.pontos.value,
+  };
 
-    const form = document.querySelector("form")
-  
-    const tarefa ={
-      titulo : form.titulo.value,
-      descricao : form.descricao.value,
-      pontos : form.pontos.value
-    }
-  
-    validar(tarefa)
-    console.log(titulo, descricao, pontos);
-  })
-  
-  function validar(tarefa) {
-      limparErros();
-    if (tarefa.titulo == "") {
-      document.querySelector("#titulo").classList.add("is-error")
-      document.querySelector("#titulo-error").innerText = "O título é obrigatório"
-    }
-    if (tarefa.descricao == "") {
-      document.querySelector("#descricao").classList.add("is-error")
-      document.querySelector("#descricao-error").innerText = "A descricao é obrigatória"
-    }
-   }
-  
-   function limparErros() {
-      document.querySelector("#titulo").classList.remove("is-error")
-      document.querySelector("#titulo-error").innerText = ""
-   }
+  if (validar(tarefa)) {
+    console.log(tarefa);
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    tarefas.push(tarefa);
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+
+    window.location = "/";
+  }
+});
+
+function validar(tarefa) {
+  let valid = true;
+  limparErros();
+  if (tarefa.titulo == "") {
+    document.querySelector("#titulo").classList.add("is-error");
+    document.querySelector("#titulo-error").innerText =
+      "O tipo do treino é obrigatório";
+    valid = false;
+  }
+  if (tarefa.descricao == "") {
+    document.querySelector("#descricao").classList.add("is-error");
+    document.querySelector("#descricao-error").innerText =
+      "A descricao é obrigatória";
+    valid = false;
+  }
+  if (tarefa.pontos <= 0) {
+    document.querySelector("#pontos").classList.add("is-error");
+    document.querySelector("#pontos-error").innerText =
+      "Dificuldade deve ser maior do que 0";
+    valid = false;
+  }
+  return valid;
+}
+
+function limparErros() {
+  document
+    .querySelectorAll(".nes-input.is-error, .nes-textarea.is-error")
+    .forEach((campo) => campo.classList.remove("is-error"));
+
+  document
+    .querySelectorAll("span.is-error")
+    .forEach((span) => (span.innerText = ""));
+}
---
+++ js/carregar.js
@@ -0,0 +1,31 @@
+console.log("carregar dados");
+
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+tarefas.forEach((tarefa) => {
+  const card = document.createElement("div");
+  card.classList.add("nes-container", "with-title", "is-centered");
+
+  card.innerHTML = `<p class="title">${tarefa.titulo}</p>
+    <p>${tarefa.descricao}</p>
+
+    <a href="#" class="nes-badge is-icon">
+          <span class="is-warning"><i class="nes-icon trophy is-small"></i></span>
+          <span class="is-warning">${tarefa.pontos}</span>
+        </a>
+    
+        <progress
+        class="nes-progress"
+        value="50"
+        max="100"
+      ></progress>
+    <!-- <details>
+                <button type="button" class="nes-btn is-error">Apagar</button>
+                <button type="button" class="nes-btn is-primary">-</button>
+                <button type="button" class="nes-btn is-primary">+</button>
+    </details> -->
+    <button type="button" class="nes-btn is-error">Apagar</button>
+    <button type="button" class="nes-btn is-normal">-</button>
+    <button type="button" class="nes-btn is-normal">+</button>`;
+  document.querySelector("#lista-de-tarefas").appendChild(card);
+});
